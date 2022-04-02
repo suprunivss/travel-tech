a4e307dc4b9f3f240d2ce0abd86172868309bb85
Martusin <suprunivss@gmail.com>
2022-04-03 02:49:52 +0300

    feat: Add input component

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,10 @@
-const Input = () => {
-  return (
-    <div>
+import './Input.scss'
 
+const Input = (props) => {
+  return (
+    <div className='input'>
+      <input type='text' placeholder={props.placeholder} className='input__text'/>
+      <button className='input__button'/>
     </div>
   );
 };
